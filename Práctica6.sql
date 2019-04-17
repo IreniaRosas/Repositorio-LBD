94c13dbc8f32fd4650c489cf6b157c3a6688fd7f
IreniaRosas <47844243+IreniaRosas@users.noreply.github.com>
2019-04-17 17:39:49 -0500

    Add files via upload

---
+++ Práctica6.sql
@@ -0,0 +1,85 @@
+/* Funciones de agregación*/
+
+use RentaVideo
+select * from Película
+
+select COUNT(Nombre) from Película
+where año > 2000
+
+select SUM(CopiasPorUnidad) from Película
+
+select MAX(PrecioPorUnidad) from Película
+
+select COUNT(CopiasPorUnidad)  from Película 
+where CopiasPorUnidad < 12 
+
+select Genero, COUNT(Nombre) as Num_Películas from Película
+GROUP BY Genero
+
+select Genero, COUNT(Nombre) as Num_Películas from Película
+GROUP BY Genero
+HAVING SUM(CopiasPorUnidad) > 20
+
+select Proveedor, COUNT(Nombre) AS TotalPelis_Entregadas from Película
+GROUP BY Proveedor
+
+
+
+-------------------------------------------
+
+select *from Cliente
+
+select COUNT(Id_Cliente) from Cliente
+
+-------------------------------------------
+
+select *from Empleado
+
+select MAX(FechaIngreso) from Empleado
+
+select MIN(FechaIngreso) from Empleado
+
+
+
+-------------------------------------------
+
+select *from Renta
+
+select SUM(Total_Articulos) from Renta
+
+select count(Precio_Total) from Renta
+where Precio_Total <250
+
+select COUNT(Id_Renta) from Renta
+where Empleado = 1001
+
+select Empleado, SUM(Total_Articulos) AS Peliculas_Vendidas from Renta
+GROUP BY Empleado
+
+Select Empleado, COUNT(Cliente) As Clientes_Atendidos from Renta
+GROUP BY Empleado
+
+Select Empleado, COUNT(Cliente) As Clientes_Atendidos from Renta
+GROUP BY Empleado
+HAVING SUM(Total_Articulos) > 5
+
+
+
+-------------------------------------------
+
+select *from Género
+
+select COUNT(Nombre) from Género
+
+-------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
